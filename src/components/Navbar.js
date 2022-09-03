2f2deef3d8a133ea73aa3f68c927936011877d5f
BakaramAnushaReddy <bakaramanusha2003@gmail.com>
2022-09-03 14:11:16 +0530

    make it better

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ export default function Navbar() {
   
   <nav className="navbar navbar-expand-lg navbar-light bg-info">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">NeilCodesBoom</Link>
+    <Link className="navbar-brand" to="/">Codes</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
--- src/context/codes/CodeState.js
+++ src/context/codes/CodeState.js
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const CodeState = (props) => {
   let navigate = useNavigate();
   // const host = "http://localhost:5000";
-  const host="https://neilcodesboombackend.herokuapp.com"
+  const host="https://codyminderbackend.herokuapp.com"
 
   //for present state
 
